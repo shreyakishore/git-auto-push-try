10212d2884ed8270a2ce76af25204f32ce1b3486
shreyakishore <shreyakishore5@gmail.com>
2021-09-14 06:03:27 +0000

    "auto push"

--- main/__init__.py
+++ main/__init__.py
@@ -3,10 +3,13 @@ import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
+
+
 def some_job():
     subprocess.call(['git', 'add', '-A'])
     subprocess.call(['git', 'commit', '-m', '"auto push"'])
     subprocess.call(['git', 'push', 'origin', 'master', '--force'])
+
 
 scheduler = BlockingScheduler()
 scheduler.add_job(some_job, 'interval', seconds=10)
