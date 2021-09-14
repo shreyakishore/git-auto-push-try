24714b8e7991235bb90fd92c55f1447e217cb057
shreyakishore <shreyakishore5@gmail.com>
2021-09-14 05:09:27 +0000

    "auto push"

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN apt-get -y install git
 #RUN git config --global user.name "shreyakishore"
 #RUN git config --global user.email "shreyakishore5@gmail.com"
 #RUN git config --global user.password "AnuradhaKass5"
-RUN git init
-RUN git remote add origin https://shreyakishore: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@github.com/shreyakishore/git-auto-push-try.git
+#RUN git init
+#RUN git remote add origin https://shreyakishore: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@github.com/shreyakishore/git-auto-push-try.git
 CMD ["python", "-m", "main.__init__"]
--- main/__init__.py
+++ main/__init__.py
@@ -4,17 +4,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 def some_job():
-    subprocess.call(['ls', '-la'])
-    # subprocess.call(['cat','~/.gitconfig'])
-    subprocess.call(['git', 'config', '--global', 'user.name', '"shreyakishore"'])
-    subprocess.call(['git', 'config', '--global', 'user.email', '"shreyakishore5@gmail.com"'])
-    subprocess.call(['git', 'config', '--global', 'user.password', '"AnuradhaKass5"'])
-    # subprocess.call(['cat', 'gitpush.sh'])
-    # subprocess.call(['sh', './gitpush.sh'])
     subprocess.call(['git', 'add', '-A'])
     subprocess.call(['git', 'commit', '-m', '"auto push"'])
-    subprocess.call(['git', 'push', 'origin', 'master'])
-
+    subprocess.call(['git', 'push', 'origin', 'master', '--force'])
 
 scheduler = BlockingScheduler()
 scheduler.add_job(some_job, 'interval', seconds=10)
